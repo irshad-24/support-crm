cb8fe270151418d59fbf32bf0f312df7dd708979
Shaikh Irshad <irshad240803@gmail.com>
2026-08-01 00:31:07 +0530

    Connect frontend to deployed backend

--- support-crm/frontend/src/services/api.js
+++ support-crm/frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/api",
+  baseURL: "https://support-crm-production-2a43.up.railway.app/api",
 });
 
 export default api;
